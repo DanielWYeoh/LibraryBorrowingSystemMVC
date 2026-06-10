87af02a26d0dd2bee230414afebcb9221ec8bbdb
masjohncook <mas.john.cook@gmail.com>
2026-06-10 14:25:11 +0700

    fix - conflict in merge

--- src/LibraryBorrowingSystem/BorrowLimitExceededException.java
+++
@@ -1,8 +0,0 @@
-package LibraryBorrowingSystem;
-
-/** Thrown when a member tries to borrow beyond their allowed borrow limit. */
-public class BorrowLimitExceededException extends RuntimeException {
-    public BorrowLimitExceededException(String message) {
-        super(message);
-    }
-}
